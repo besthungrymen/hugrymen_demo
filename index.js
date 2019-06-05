f97f1809ae3e73fa2a5722064fa4d38f7a76eb0b
Hanzhi Yang <tcoherence@gmail.com>
2019-06-05 13:35:33 -0500

    change index features

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 $('#search').keypress(function (e) {
     url = "./main_page.html"
     if (e.keyCode == 13) {
-        
+        // get the value in the input check every character to see if it is valid
+        var valueinput = $('#search').val();
+        // for (let i = 0; i < valueinput.length; i++ ) {
+        //     let c = valueinput.charAt(i);
+        //     if ( !validCharForStreetAddress(c) ) {
+        //         alert("Please input valid address!");
+        //         return true;
+        //     }
+        // }
+        if ( valueinput.length == 0 ) {
+            alert("Please input your address!");
+            return true;
+        }
+        if ( !validCharForStreetAddress(valueinput) ) {
+            alert("Please input valid address!");
+            return true;
+        }
         location.href = url;
         return false
     }
 });
 
+$('#searchBtn').click(function(){
+    var valueinput = $('#search').val();
+    // for (let i = 0; i < valueinput.length; i++ ) {
+    //     let c = valueinput.charAt(i);
+    //     if ( !validCharForStreetAddress(c) ) {
+    //         alert("Please input valid address!");
+    //         return true;
+    //     }
+    // }
+    if ( valueinput.length == 0 ) {
+        alert("Please input your address!");
+        return true;
+    }
+    if ( !validCharForStreetAddress(valueinput) ) {
+        alert("Please input valid address!");
+        return true;
+    }
+    location.href = url;
+    return false
+});
+
 function validCharForStreetAddress(c) {
-    return ",#-/ !@$%^*(){}|[]\\".indexOf(c) >= 0;
+    return ",#-/! @$%^*(){}|[]\\".indexOf(c) >= 0;
 }
