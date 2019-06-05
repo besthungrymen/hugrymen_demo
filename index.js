f6187b8e5c00f62f081102bccbead3afbb98c52d
Hanzhi Yang <tcoherence@gmail.com>
2019-06-05 12:36:36 -0500

    cart changing, connot write to local file

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-
-
 $('#search').keypress(function (e) {
     url = "./main_page.html"
     if (e.keyCode == 13) {
+        
         location.href = url;
         return false
     }
-    
 });
+
+function validCharForStreetAddress(c) {
+    return ",#-/ !@$%^*(){}|[]\\".indexOf(c) >= 0;
+}
